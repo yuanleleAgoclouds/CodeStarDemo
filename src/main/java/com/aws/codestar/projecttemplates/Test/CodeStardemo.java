7aff45c2a3a34564caece11836085019c320713f
袁乐乐 <yuanlele@goclouds.cn>
2018-12-14 15:21:51 +0800

    Test

---
+++ src/main/java/com/aws/codestar/projecttemplates/Test/CodeStardemo.java
@@ -0,0 +1,10 @@
+package com.aws.codestar.projecttemplates.Test;
+
+/**
+ * Created by goclouds on 2018/12/14.
+ */
+public class CodeStardemo {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
